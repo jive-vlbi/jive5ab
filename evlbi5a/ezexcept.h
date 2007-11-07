9df27c0e7a7a8fa1afacb103e6339d31f728eec9
jive_cc <>
2007-11-07 16:19:37 +0000

    HV: Provides macros for easy exception declaring/defining

---
+++ evlbi5a/ezexcept.h
@@ -0,0 +1,54 @@
+// use this to define/declare exceptions of your own type
+// they all take "const std::string&" as c'tor arg and are
+// derived off std::exception.
+// Usagage:
+//   in .h file:
+//   #include <ezexcept.h>
+//   DECLARE_EZEXCEPT(Whatchamacallem);
+// 
+//   in .cc
+//   DEFINE_EZEXCEPT(Whatchamacallem);
+//   ...
+///  if( usr_fscked_up )
+//     throw Whatchamacallem("Usr fscked up!");
+#ifndef JIVE5A_EZEXCEPT_H
+#define JIVE5A_EZEXCEPT_H
+
+#include <exception>
+#include <string>
+#include <sstream>
+
+// Declare the prototype of the exception
+#define DECLARE_EZEXCEPT(xept) \
+    struct xept:\
+        public std::exception\
+    {\
+        xept( const std::string& m );\
+        virtual const char* what( void ) const throw();\
+        virtual ~xept() throw();\
+        const std::string __m;\
+    };
+
+// For the xtra-lzy ppl: throws + prefixed with
+// FILE:LINE
+#define THROW_EZEXEPT(xept, msg) \
+    do { int eZ_Lijn_gH = __LINE__; \
+         const std::string eZ_Fijl_9q( __FILE__ );\
+         std::ostringstream eZ_Stroom_yw;\
+        eZ_Stroom_yw << ez_Fijl_9q << ":" << ez_Lijn_gH << " - " << msg;\
+        throw xept(ez_Stroom_yw.str());\
+        while( 0 );
+
+// And this is to go in your .cc file
+#define DEFINE_EZEXCEPT(xept) \
+    xept::xept( const std::string& m ):\
+        __m( m )\
+    {}\
+    const char* xept::what( void ) const throw() {\
+        return __m.c_str();\
+    }\
+    xept::~xept() throw() \
+    {}
+
+
+#endif
