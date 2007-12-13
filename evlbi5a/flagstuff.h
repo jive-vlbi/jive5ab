1f5caad118c6172db25a186b2301896cc8e46f67
jive_cc <>
2007-12-13 09:27:12 +0000

    HV: * the exceptions are now 'templated' as well so you       don't have to actually compile anything for this module       in order to use it. Just #include <flagstuff.h> and       you're all set     * Made the multiple-initialization-property a template-parameter       so it becomes part of the type so you cannot mix up       multiple/single initializable flagsets.     * Some cosmetic changes.

--- evlbi5a/flagstuff.h
+++ evlbi5a/flagstuff.h
@@ -17,8 +17,10 @@
 // Note: we make them templates - seems a bit over-the-top
 //       but it allows us to get away with multiple instantiations
 //       i.e. we don't have to split up this code in a .cc and .h
-//       file; basically everyting is a template so it would be
-//       bad form to make ppl build the .cc ... :)
+//       file. If not templated, then everyting but this exception
+//       is a template so the user would have to compile&link the
+//       .cc file into his/her app. Now, it being a template too,
+//       usr can just #include<> and be done with it!
 #define DEFINEEXCEPTION(xept) \
     template <typename T = void> \
     struct xept:\
@@ -72,6 +74,8 @@ struct flagdescr_type {
     const std::string __nm;
 };
 
+
+
 // template parameters are the enum-type E
 // and the base-type for flags (int, unsigned short, ...), F.
 // Note: all instances of a particular flagset_type<> share
@@ -79,15 +83,18 @@ struct flagdescr_type {
 //       modify the mapping at runtime - just be aware that
 //       it may affect setting/clearing of flags for *all*
 //       instances of that type, existing and yet-to-be created.
-// Modify/init the map of known flags via the static member functions.
-// You can let the system throw if you try to reinit it [>once, that is]
-template <typename E, typename F>
+// Since it's a static datamember, you have the choice to allow
+// multiple initialization of this mapping of recognized flags.
+// By adding it to the template-parameter-list you get a different
+// type for multiple/singularly initializable objects, which is
+// obviously much safer than, eg. specifying it on a per-call basis.
+template <typename E, typename F, bool AllowMultipleInit=false>
 class flagset_type {
     public:
-        // a flag descriptor (bits + name)
-        typedef flagdescr_type<F>                                        flag_descr_type;
+        // provide a typedef for the flagdescr_type
+        typedef flagdescr_type<F>               flag_descr_type;
         // the map type for mapping enum => descriptor
-        typedef std::map<E, typename flagset_type<E,F>::flag_descr_type> flag_map_type;
+        typedef std::map<E, flagdescr_type<F> > flag_map_type;
 
 
         // Constructor.
@@ -102,12 +109,12 @@ class flagset_type {
         {}
 
         // copy c'tor
-        flagset_type( const flagset_type<E,F>& other ):
+        flagset_type( const flagset_type<E,F,AllowMultipleInit>& other ):
             flags( other.flags )
         {}
 
         // and assignment
-        const flagset_type<E,F>& operator=( const flagset_type<E,F>& other ) {
+        const flagset_type<E,F>& operator=( const flagset_type<E,F,AllowMultipleInit>& other ) {
             if( this!=&other )
                 flags = other.flags;
             return *this;
@@ -120,7 +127,7 @@ class flagset_type {
         // fst.clr( flag2 ).set( flag1 ).set( flagn );
 
         // set the indicated flag. throw if flag not defined.
-        flagset_type<E, F>& set( const E& e ) {
+        flagset_type<E, F, AllowMultipleInit>& set( const E& e ) {
             typename flag_map_type::const_iterator  curf = flag_map.find(e);
 
             if( curf==flag_map.end() )
@@ -142,13 +149,13 @@ class flagset_type {
             return *this;
         }
         // clear all flags
-        flagset_type<E,F>& clr_all( void ) {
+        flagset_type<E,F,AllowMultipleInit>& clr_all( void ) {
             flags = (F)0;
             return *this;
         }
         // test if *any* flags are set
         bool empty( void ) const {
-            return (flags!=(F)0);
+            return (flags==(F)0);
         }
 
         // return true if flag is set ...
@@ -164,13 +171,13 @@ class flagset_type {
         // syntactic sugah.
 
         // in-place modification, set the flag 'e'
-        flagset_type<E, F>& operator|=( const E& e ) {
+        flagset_type<E, F,AllowMultipleInit>& operator|=( const E& e ) {
             return this->set( e );
         }
 
         // return new instance which is the OR of this + indicated flag
-        flagset_type<E, F>  operator|( const E& e ) const {
-            flagset_type<E,F>   rv( *this );
+        flagset_type<E, F,AllowMultipleInit>  operator|( const E& e ) const {
+            flagset_type<E,F,AllowMultipleInit>   rv( *this );
             rv.set( e );
             return rv;
         }
@@ -180,14 +187,12 @@ class flagset_type {
             return this->is_set(e);
         }
 
-
-
         // Set the map of recognized flags.
-        // If multiple initializations is disabled (false)
-        // the system will throw if initialization is attempted > once.
-        static void set_flag_map( const flag_map_type& m,
-                                  bool allowmultipleinit = false ) {
-            if( flag_map.size()>0 && allowmultipleinit==false )
+        // Depending on the value of the "AllowMultipleInit"
+        // template parameter we allow multiple initializations
+        // or not
+        static void set_flag_map( const flag_map_type& m ) {
+            if( flag_map.size()>0 && AllowMultipleInit==false )
                 throw multiple_map_initialization<>();
             flag_map = m;
             return;
@@ -210,15 +215,16 @@ class flagset_type {
 };
 
 // declare the static datamember
-template <typename E, typename F>
-typename flagset_type<E,F>::flag_map_type flagset_type<E,F>::flag_map = flagset_type<E,F>::flag_map_type();
+template <typename E, typename F, bool b>
+typename flagset_type<E,F,b>::flag_map_type flagset_type<E,F,b>::flag_map = flagset_type<E,F,b>::flag_map_type();
+
 
 // And define how a flagset_type gets written on a std::ostream
-template <typename E, typename F>
-std::ostream& operator<<(std::ostream& os, const flagset_type<E,F>& fst) {
-    bool                                                       comma( false );
-    const typename flagset_type<E,F>::flag_map_type&           fm( flagset_type<E,F>::get_flag_map() );
-    typename flagset_type<E,F>::flag_map_type::const_iterator  curflag;
+template <typename E, typename F,bool b>
+std::ostream& operator<<(std::ostream& os, const flagset_type<E,F,b>& fst) {
+    bool                                                         comma( false );
+    const typename flagset_type<E,F,b>::flag_map_type&           fm( flagset_type<E,F,b>::get_flag_map() );
+    typename flagset_type<E,F,b>::flag_map_type::const_iterator  curflag;
 
     os << "<";
     // Loop over all defined flags and see if they're set
