400e8956e83883c06d9535a0983cd745f9c330ed
jive_cc <>
2008-10-13 14:40:43 +0000

    HV: imported byte-swapping stuff from pcint; we need to swap     bytes when decoding JCCS messages (ROT broadcasts, eg)

---
+++ evlbi5a/byteorder.cc
@@ -0,0 +1,270 @@
+// implementation
+// Copyright (C) 2007-2008 Harro Verkouter
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Author:  Harro Verkouter - verkouter@jive.nl
+//          Joint Institute for VLBI in Europe
+//          P.O. Box 2
+//          7990 AA Dwingeloo
+#include <byteorder.h>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+byteorder_t getHostByteOrder( void ) {
+    const int     sz( sizeof(unsigned int) );
+
+    typedef union _test {
+        unsigned int    l;
+        unsigned char   c[ sz ];
+    } test;
+
+    test         t;
+    byteorder_t  retval( unknownOrder );
+
+    // set the 'unsigned int' member to 1 so we can
+    // test in which byte the one ends up
+    t.l = 0x1;
+
+    // Let's figure out what kinda host this is
+    if( (t.c[(sz-1)]==0x1) ) {
+        // the '0x1' ended up in the high-order-byte
+        retval = bigEndian;
+    } else if( t.c[0]==0x1 ) {
+        // the '0x1' ended up in the lowest order byte
+        retval = littleEndian;
+    }
+    return retval;
+}
+
+//
+// The swap-stuff
+//
+// NOTE NOTE NOTE NOTE
+//
+// These implementations are definitely not
+// the fastest around but (hopefully) the easiest
+// to read and portable...
+//
+
+// For 2-byte quantities there's little overhead in
+// always using a copy.. presumably testing whether
+// or not to use a temporary takes as long
+// as copying the stuff across... :)
+//
+// Anyway... all of these 'zwabbers' are hopefully "smart"
+// enough to figure out wether or not it is an
+// in-place swap (ie use a temporary) or not..
+//
+// Update - the zwabbers used to be smart enough
+//          to detect even overlapping swaps (src-area
+//          and dst-area partially overlapping). However,
+//          this seems very unlikely to be able to happen...
+//          [can't think of a case where this could be
+//           useful/desired]. In short: *that* specific
+//          smartness was removed. Still (attempts to) detect
+//          and support in-place swaps.
+//
+// Update 2:
+//   HV: 03/09/2004 - Removed *all* smartness from these things.
+//                    It's taken care of at a different level.
+//
+template <>
+void zwabber<2ULL>::operator()( void* dst, const void* src ) const {
+    unsigned char*         dp = static_cast<unsigned char*>(dst);
+    const unsigned char*   cp = static_cast<const unsigned char*>(src);
+
+    dp[0] = cp[1];
+    dp[1] = cp[0];
+    return;
+}
+
+template <>
+void zwabber<4ULL>::operator()( void* dst, const void* src ) const {
+    unsigned char*         dp = static_cast<unsigned char*>(dst);
+    const unsigned char*   cp = static_cast<const unsigned char*>(src);
+
+    dp[0] = cp[3];
+    dp[1] = cp[2];
+    dp[2] = cp[1];
+    dp[3] = cp[0];
+
+    return;	
+}
+
+// 8-byte quantities
+template <>
+void zwabber<8ULL>::operator()( void* dst, const void* src ) const {
+    unsigned char*         dp = static_cast<unsigned char*>(dst);
+    const unsigned char*   cp = static_cast<const unsigned char*>(src);
+    dp[0] = cp[7];
+    dp[1] = cp[6];
+    dp[2] = cp[5];
+    dp[3] = cp[4];
+    dp[4] = cp[3];
+    dp[5] = cp[2];
+    dp[6] = cp[1];
+    dp[7] = cp[0];
+
+    return;	
+}
+
+// Need specializations for the mover stuff.
+// Could possibly be more intelligent than
+// this...
+template <>
+void mover<2ULL>::operator()( void* dst, const void* src ) const {
+    ::memmove(dst, src, 2 );
+}
+template <>
+void mover<4ULL>::operator()( void* dst, const void* src ) const {
+    ::memmove(dst, src, 4 );
+}
+template <>
+void mover<8ULL>::operator()( void* dst, const void* src ) const {
+    ::memmove(dst, src, 8 );
+}
+
+
+// the zwab operator()
+void zwab::func( void* dst, const void* src,
+        unsigned long long int n ) const
+{
+    switch( n ) {
+        case 2ULL:
+            zwabber<2ULL>()(dst, src);
+            break;
+
+        case 4ULL:
+            zwabber<4ULL>()(dst, src);
+            break;
+
+        case 8ULL:
+            zwabber<8ULL>()(dst, src);
+            break;
+
+        default:
+            ostringstream s;
+            s << "zwab::operator()(void*, const void*, " << n << ")/Unsupported "
+                << "number of bytes to swap!";
+            throw( s.str().c_str() );
+    }
+}
+
+// and the move operator()
+void move::func( void* dst, const void* src,
+        unsigned long long int n ) const
+{
+    // if src and dst are the same (and we are the mover!!)
+    // there's really nothing to be done...
+    if( dst==src )
+        return;
+
+    switch( n ) {
+        case 2ULL:
+            mover<2ULL>()(dst, src);
+            break;
+
+        case 4ULL:
+            mover<4ULL>()(dst, src);
+            break;
+
+        case 8ULL:
+            mover<8ULL>()(dst, src);
+            break;
+
+        default:
+            ostringstream s;
+            s << "move::operator()(void*, const void*, " << n << ")/Unsupported "
+                << "number of bytes to move!";
+            throw( s.str().c_str() );
+    }
+}
+
+// The endian_converter thingy
+endian_converter::endian_converter() {}
+
+endian_converter::endian_converter( byteorder_t dst, byteorder_t src ) {
+    if( dst==mimicHost )
+        dst = getHostByteOrder();
+    if( src==mimicHost )
+        src = getHostByteOrder();
+    if( src==unknownOrder || dst==unknownOrder )
+        throw( "endian_converter(dst, src)/ Either dst||src == unknownOrder" );
+
+    if( src==dst )
+        myac = new move();
+    else
+        myac = new zwab();
+}
+
+endian_converter::~endian_converter() {}
+
+template <>
+void endian_converter::operator()<char>( char& ) const
+{}
+
+template <>
+void endian_converter::operator()<char>( char& d, const char& s ) const {
+    (&d!=&s) && (d=s);
+    return;
+}
+
+template <>
+void endian_converter::operator()<char>( char* d, const char* s, unsigned int n ) const {
+    (d!=s) && ::memmove(d, s, n);
+    return;
+}
+
+template <>
+void endian_converter::operator()<unsigned char>( unsigned char& ) const
+{}
+
+template <>
+void endian_converter::operator()<unsigned char>( unsigned char& d, const unsigned char& s ) const {
+    (&d!=&s) && (d=s);
+    return;
+}
+
+template <>
+void endian_converter::operator()<unsigned char>( unsigned char* d, const unsigned char* s, unsigned int n ) const {
+    (d!=s) && ::memmove(d, s, n);
+    return;
+}
+
+ostream& operator<<( ostream& os, const byteorder_t& o ) {
+    switch( o ) {
+        case bigEndian:
+            os << "BE";
+            break;
+
+        case littleEndian:
+            os << "LE";
+            break;
+
+        case mimicHost:
+            os << "MimicHost[" << getHostByteOrder() << "]";
+            break;
+
+        case unknownOrder:
+            os << "??";
+            break;
+
+        default:
+            os << "<INVALID BYTE ORDER>";
+            break;
+    }
+    return os;
+}
---
+++ evlbi5a/byteorder.h
@@ -0,0 +1,229 @@
+// Byte-order related stuff / taken from my own PCInt project.
+// Copyright (C) 2007-2008 Harro Verkouter
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Author:  Harro Verkouter - verkouter@jive.nl
+//          Joint Institute for VLBI in Europe
+//          P.O. Box 2
+//          7990 AA Dwingeloo
+//
+#ifndef JIVE5A_BYTEORDER_H
+#define JIVE5A_BYTEORDER_H
+
+#include <iostream>
+#include <countedpointer.h>
+
+
+typedef enum _byteorder_t {
+    // the (for us) important byte-orderings.
+    // mimicHost is supposed to be able to be used
+    // as a place-holder for the .. well, you can
+    // figure that out for yourself.. :)
+    bigEndian, littleEndian, mimicHost, unknownOrder
+} byteorder_t;
+// display it in HumanReadableFormat
+std::ostream& operator<<( std::ostream& os, const byteorder_t& o );
+
+// retrieve the hostByteOrder
+// Can be:
+//   - bigEndian [eg Sun/SPARC,HP]
+//   - littleEndian [intel x86]
+//   - unknownOrder [ ... ] machines that we cannot determine it for...
+//
+// NEVER returns mimicHost (can end up in infinite loop otherwise)
+byteorder_t getHostByteOrder( void );
+
+// zwabber swaps a number of bytes [the template parameter]
+// Default implementation just ain't there.
+// (partial) template specializations for recognized- 
+// byte-size items exist. Hence, if the usr
+// requests an item of unrecognized size to be 
+// swapped, it will result in a compile/link error...
+template <unsigned long long int>
+struct zwabber {
+    void operator()( void* dst, const void* dst ) const;
+};
+
+// mover just moves a number of bytes.
+// see above for recognized byte-size-items discussion etc.
+template <unsigned long long int>
+struct mover {
+    void operator()( void* dst, const void* dst ) const;
+};
+
+
+// This struct acts as an abstract baseclass for accessors. 
+// This thang features a virtual private method
+// which does the actual hard work.
+struct accessor {
+    // define the non-virtual interface, takes care of typesafety
+    template <typename T>
+    void operator()( T& s, const T& t ) const {
+        // be smart about in-place processing or not
+        // [in-place requires a temporary]
+        if( &s==&t ) {
+            T   tmp( t );
+            this->func((void*)&s, (const void*)&tmp, sizeof(T));
+        } else {
+            this->func((void*)&s, (const void*)&t, sizeof(T));
+        }
+        return;
+    }
+
+    // process multiple items
+    template <typename T>
+    void operator()( T* d, const T* s, unsigned int n ) const {
+        // cd = current destination
+        // cs = current source
+        if( d==s ) {
+            // if doing an array in-place, only use one tmp-variable
+            T        tmp;
+            T*       cd = d;
+            const T* cs = s;
+
+            while( n-- ) {
+                tmp = *cs++;
+                this->func(cd++, &tmp, sizeof(T));
+            }
+        } else {
+            T*       cd = d;
+            const T* cs = s;
+
+            while( n-- )
+                this->func(cd++, cs++, sizeof(T));
+        }
+        return;
+    }
+
+    virtual ~accessor() {}
+
+    private:
+        // the actual hard work will be delegated to this
+        // type-unsafe function!
+        virtual void func( void* dst, const void* src,
+                           unsigned long long int n ) const = 0;
+};
+
+struct zwab:
+    public accessor
+{
+    virtual ~zwab() {}
+    private:
+    virtual void func( void* dst, const void* src,
+            unsigned long long int n ) const;
+};
+
+struct move:
+    public accessor
+{
+    virtual ~move() {}
+    private:
+    virtual void func( void* dst, const void* src,
+            unsigned long long int n ) const;
+};
+
+// This is the converter thingy that actually presents the 
+// type-safe interface to the user based on desired 
+// source+destination byteorder
+struct endian_converter {
+    public:
+        // default c'tor 
+        endian_converter();
+
+        // configure for a particular combination of
+        // src- and dst-byteorder
+        endian_converter( byteorder_t dst, byteorder_t src=mimicHost );
+
+        // define the interface and methods for the converter
+
+        // in-place conversion
+        template <typename T>
+        void operator()( T& d ) const {
+            if( !myac )
+                throw( "endian_converter::operator()(d) called on default object" );
+            (*myac)(d, d);
+            return;
+        }
+
+        // operate on a single item
+        template <typename T>
+        void operator()( T& d, const T& s ) const {
+            if( !myac )
+                throw( "endian_converter::operator()(d, s) called on default object" );
+            (*myac)(d, s);
+            return;
+        }
+
+        // operate on a number of items
+        template <typename T>
+        void operator()( T* d, const T* s, unsigned int n ) const {
+            if( !myac )
+                throw( "endian_converter::operator()(d, s, n) called on default object" );
+            (*myac)(d, s, n);
+            return;
+        }
+
+
+        // destroy allocated resources
+        ~endian_converter();
+
+    private:
+        countedpointer<accessor>   myac;
+};
+
+// byte-sized items shouldn't be zwabbed ...
+// but the must be accepted by the system...
+template <>
+void endian_converter::operator()<char>( char& d ) const;
+
+template <>
+void endian_converter::operator()<char>( char& d, const char& s ) const;
+
+template <>
+void endian_converter::operator()<char>( char* d, const char* s, unsigned int n ) const;
+
+template <>
+void endian_converter::operator()<unsigned char>( unsigned char& d ) const;
+
+template <>
+void endian_converter::operator()<unsigned char>( unsigned char& d, const unsigned char& s ) const;
+
+template <>
+void endian_converter::operator()<unsigned char>( unsigned char* d, const unsigned char* s, unsigned int n ) const;
+
+
+// specializations must be seen by the compiler at compile-time,
+// that's why they're listed here
+
+// specializations for two-byte-sized items
+template <>
+void zwabber<2ULL>::operator()( void* dst, const void* src ) const;
+
+template <>
+void mover<2ULL>::operator()( void* dst, const void* src ) const;
+
+// id. for four-byte-sized items
+template <>
+void zwabber<4ULL>::operator()( void* dst, const void* src ) const;
+
+template <>
+void mover<4ULL>::operator()( void* dst, const void* src ) const;
+
+// id. for eight-byte-sized items
+template <>
+void zwabber<8ULL>::operator()( void* dst, const void* src ) const;
+
+template <>
+void mover<8ULL>::operator()( void* dst, const void* src ) const;
+#endif
