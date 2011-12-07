d35032e12e76455707dd5b0f7e114612e0277ecf
jive_cc <>
2011-12-07 15:01:28 +0000

    HV: * atomic.h  defines atomic inc/dec and try-set (to emulate locking)       on values of 1, 2 and 4 bytes. Part of "block + blockpool_type       refcounting", actually     * jive5a_bcd.h  defines to/from BCD encodings. Throws upon fishy digits,       encoded number doesn't fit into the destination, decoded digit > 9.

---
+++ evlbi5a/atomic.h
@@ -0,0 +1,231 @@
+// Provide atomic updates to integral values of size of 1, 2 or 4 bytes.
+#ifndef JIVE5AB_ATOMIC_H
+#define JIVE5AB_ATOMIC_H
+// Copyright (C) 2007-2011 Harro Verkouter
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Author:  Harro Verkouter - verkouter@jive.nl
+//          Joint Institute for VLBI in Europe
+//          P.O. Box 2
+//          7990 AA Dwingeloo
+//
+// Atomically update counters. All methods keep on trying until succesfull,
+// except the "bool atomic<T>.update()", which tries exactly once.
+// The latter's main use is when attempting to "grab a lock" - when you,
+// irrespective of the current value, want to try to go atomically from "0"
+// -> "1" (e.g.). This will only succeed if the current value WAS 0.
+//
+// What to do when the atomic update fails is left up the the caller.
+//
+//
+// The code has been split into a typesafe part ("atomic<T>") and a
+// type-unsafe but appropriate for the addressed size piece of code.
+//
+// Accessing non-integral types or accessing datatypes of unsupported size
+// will result, in principle, in compilation errors.
+//
+#include <stdint.h>
+
+struct xchg_dummy { unsigned char a[100]; };
+#define XCHG_DUMMY(x)  ((struct xchg_dummy*)(x))
+
+// Intel 32bit/64bit architecture
+#if defined(__i386__) || defined(__x86_64__)
+
+#define CAS1 "lock; cmpxchgl %b1,%b2"
+#define CAS2 "lock; cmpxchgl %w1,%w2"
+#define CAS4 "lock; cmpxchgl %k1,%k2"
+
+// Atomically add 'a' to '*ptr' return prev value in 'p'
+#define ATOMIC_ADD(ptr, o, n, a, p, instr) \
+        o = *ptr; \
+        n = o+a; \
+        __asm__ __volatile__( instr \
+                                : "=a"(p) \
+                                : "q"(n), "m"(*XCHG_DUMMY(ptr)), "0"(o) \
+                                : "memory" ); 
+
+// Id. but now subtraction
+#define ATOMIC_SUB(ptr, o, n, a, p, instr) \
+        o = *ptr; \
+        n = o-a; \
+        __asm__ __volatile__( instr \
+                                : "=a"(p) \
+                                : "q"(n), "m"(*XCHG_DUMMY(ptr)), "0"(o) \
+                                : "memory" );
+
+// atomically try to set *ptr to 'n', succeeds if *ptr==o. store previous
+// value in 'p'
+#define ATOMIC_SET(ptr, o, n, p, instr) \
+        __asm__ __volatile__( instr \
+                                : "=a"(p) \
+                                : "q"(n), "m"(*XCHG_DUMMY(ptr)), "0"(o) \
+                                : "memory" ); 
+
+
+#define MKINCFUNC(type, instr) \
+    static inline type atomic_inc(type volatile* ptr) { \
+        volatile type oud, nieuw, vorig; \
+        do { \
+            ATOMIC_ADD(ptr, oud, nieuw, 1, vorig, instr); \
+        } while( vorig!=oud ); \
+        return nieuw; \
+    }
+
+#define MKDECFUNC(type, instr) \
+    static inline type atomic_dec(type volatile* ptr) { \
+        volatile type oud, nieuw, vorig; \
+        do { \
+            ATOMIC_SUB(ptr, oud, nieuw, 1, vorig, instr); \
+        } while( vorig!=oud ); \
+        return nieuw; \
+    }
+
+
+
+MKINCFUNC(int8_t, CAS1)
+MKINCFUNC(uint8_t, CAS1)
+MKDECFUNC(int8_t, CAS1)
+MKDECFUNC(uint8_t, CAS1)
+
+MKINCFUNC(int16_t, CAS2)
+MKINCFUNC(uint16_t, CAS2)
+MKDECFUNC(int16_t, CAS2)
+MKDECFUNC(uint16_t, CAS2)
+
+MKINCFUNC(int32_t, CAS4)
+MKINCFUNC(uint32_t, CAS4)
+MKDECFUNC(int32_t, CAS4)
+MKDECFUNC(uint32_t, CAS4)
+
+
+#define MKADDFUNC(type, instr) \
+    static inline type atomic_add(type volatile* ptr, const type toadd) { \
+        volatile type oud, nieuw, vorig; \
+        do { \
+            ATOMIC_ADD(ptr, oud, nieuw, toadd, vorig, instr); \
+        } while( vorig!=oud );\
+        return nieuw; \
+    }
+
+#define MKSUBFUNC(type, instr) \
+    static inline type atomic_sub(type volatile* ptr, const type tosub) { \
+        volatile type oud, nieuw, vorig; \
+        do { \
+            ATOMIC_SUB(ptr, oud, nieuw, tosub, vorig, instr); \
+        } while( vorig!=oud );\
+        return nieuw; \
+    }
+
+
+MKADDFUNC(int8_t, CAS1)
+MKADDFUNC(uint8_t, CAS1)
+MKSUBFUNC(int8_t, CAS1)
+MKSUBFUNC(uint8_t, CAS1)
+
+MKADDFUNC(int16_t, CAS2)
+MKADDFUNC(uint16_t, CAS2)
+MKSUBFUNC(int16_t, CAS2)
+MKSUBFUNC(uint16_t, CAS2)
+
+MKADDFUNC(int32_t, CAS4)
+MKADDFUNC(uint32_t, CAS4)
+MKSUBFUNC(int32_t, CAS4)
+MKSUBFUNC(uint32_t, CAS4)
+
+#define MKTRYADDFUNC(type, instr) \
+    static inline type atomic_try_add(type volatile* ptr, const type toadd) { \
+        volatile type oud, nieuw, vorig; \
+        ATOMIC_ADD(ptr, oud, nieuw, toadd, vorig, instr); \
+        return( vorig==oud );\
+    }
+
+#define MKTRYSUBFUNC(type, instr) \
+    static inline type atomic_try_sub(type volatile* ptr, const type tosub) { \
+        volatile type oud, nieuw, vorig; \
+        ATOMIC_SUB(ptr, oud, nieuw, tosub, vorig, instr); \
+        return( vorig==oud );\
+    }
+
+
+MKTRYADDFUNC(int8_t, CAS1)
+MKTRYADDFUNC(uint8_t, CAS1)
+MKTRYSUBFUNC(int8_t, CAS1)
+MKTRYSUBFUNC(uint8_t, CAS1)
+
+MKTRYADDFUNC(int16_t, CAS2)
+MKTRYADDFUNC(uint16_t, CAS2)
+MKTRYSUBFUNC(int16_t, CAS2)
+MKTRYSUBFUNC(uint16_t, CAS2)
+
+MKTRYADDFUNC(int32_t, CAS4)
+MKTRYADDFUNC(uint32_t, CAS4)
+MKTRYSUBFUNC(int32_t, CAS4)
+MKTRYSUBFUNC(uint32_t, CAS4)
+
+
+#define MKSETFUNC(type, instr) \
+    static inline type atomic_set(type volatile* ptr, const type toset) { \
+        volatile type oud, vorig; \
+        do { \
+            oud = *ptr;\
+            ATOMIC_SET(ptr, oud, toset, vorig, instr); \
+        } while( vorig!=oud );\
+        return vorig; \
+    }
+
+MKSETFUNC(int8_t, CAS1)
+MKSETFUNC(uint8_t, CAS1)
+
+MKSETFUNC(int16_t, CAS2)
+MKSETFUNC(uint16_t, CAS2)
+
+MKSETFUNC(int32_t, CAS4)
+MKSETFUNC(uint32_t, CAS4)
+
+// Attempt once to set *ptr to 'toset' on the precondition that *ptr=='oud'
+// (this is for an atomic try-lock. If succeeds: you have it locked
+//  if it fails someone had the lock already)
+// 
+// if( atomic_try_set(ptrtolock, 1, 0) ) {
+//    // w00t!
+// } else {
+//    // bummer :-(
+// }
+#define MKTRYSETFUNC(type, instr) \
+    static inline bool  atomic_try_set(type volatile* ptr, const type toset, const type oud) { \
+        volatile type vorig; \
+        ATOMIC_SET(ptr, oud, toset, vorig, instr); \
+        return ( vorig==oud );\
+    }
+
+MKTRYSETFUNC(int8_t, CAS1)
+MKTRYSETFUNC(uint8_t, CAS1)
+
+MKTRYSETFUNC(int16_t, CAS2)
+MKTRYSETFUNC(uint16_t, CAS2)
+
+MKTRYSETFUNC(int32_t, CAS4)
+MKTRYSETFUNC(uint32_t, CAS4)
+
+#endif // __i386__
+
+
+
+// Intel 64bit x86_64
+#ifdef __x86_64__
+
+#endif
+
+#endif // JIVE5AB_ATOMIC_H
---
+++ evlbi5a/jive5a_bcd.h
@@ -0,0 +1,161 @@
+// convert BCD to numbers and numbers to BCD - little endian only
+// Copyright (C) 2007-2011 Harro Verkouter
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Author:  Harro Verkouter - verkouter@jive.nl
+//          Joint Institute for VLBI in Europe
+//          P.O. Box 2
+//          7990 AA Dwingeloo
+#ifndef JIVE5A_BCD_H
+#define JIVE5A_BCD_H
+
+#include <exception>
+#include <typeinfo>
+#include <sstream>
+#include <math.h>
+#include <errno.h>
+
+// This construction allows us to keep everything inside a single .h file
+// AND compile with "-W -Wall -Werror" and no "defined-but-unused"
+// warnings-cum-errors (compiletime) or doubly-defined symbols (linktime).
+template <typename T>
+static void throw_a_bcd(const std::string& file, int line, const std::string& msg="") {
+    struct exc : public std::exception {
+        public:
+            exc(const std::string& m) throw(): s(m) {}
+            virtual const char* what(void) const throw() { return s.c_str(); }
+            virtual ~exc() throw() {};
+            std::string s;
+    };
+    std::ostringstream strm;
+    strm << file << ":" << line << " [" << typeid(T).name() << "] " << msg;
+    throw exc(strm.str());
+}
+#define THROW_A_BCD(type, msg) throw_a_bcd<type>(__FILE__, __LINE__, msg);
+
+struct negative_argument { };
+struct argument_too_big { };
+struct not_a_BCD_digit { }; // 0 .. 9 are acceptable BCD digits
+
+// Fill bcd with the BCD representation of n,
+// filling BCD characters from the LSB -> MSB
+// (so you can easily mask off the last N bytes if required)
+template <typename Number, typename BCD>
+void bcd(const Number n, BCD& bcd) {
+    // assert that the number will fit in the BCD representation
+    // we must take the log10 of Number and ceil that
+    Number             copy_of_n( n );
+    double             log10number = ::log10((double)n);
+    unsigned int       digit;
+    const unsigned int n_bcd = sizeof(BCD)*2;
+
+    // log10( <negative number> ) = NaN
+    if( isnan(log10number) )
+        THROW_A_BCD(negative_argument, "negative numbers cannot be represented in BCD format");
+    // log10( 0 ) = -inf
+    //   but 0 (zero) is perfectly representable in BCD ...
+    if( isinf(log10number)!=0 )
+        log10number = 0.0;
+
+    // Now we can compute how many digits the number will require
+    unsigned int n_digit     = (unsigned int)::ceil(log10number);
+
+    // If ceil(log10) == log10 this means that we have exactly 10^x
+    // ie we require an extra digit in the representation
+    if( n_digit == (unsigned int)log10number )
+        n_digit++;
+    // Now, if the amount of BCD digits required isn't going fit
+    // we throw up. Each byte in bcd can hold two (2) BCD coded
+    // digits.
+    if( n_digit>n_bcd )
+        THROW_A_BCD(argument_too_big, "The number is too big too fit in the available BCD representation");
+
+    // loop until no more digits left in 'n' (the copy we made thereof)
+    // or we've used up all our BCD positions
+    for(digit=0, bcd=BCD(0); copy_of_n && digit<n_bcd; ++digit, copy_of_n /= 10)
+        bcd |= (BCD(copy_of_n%10)<<(digit*4));
+}
+
+template <typename BCD, typename Number>
+void unbcd(const BCD bcd, Number& n) {
+    bool                        shift = true;
+    Number                      old_n, decoded_digit;
+    unsigned int                digit;
+    unsigned char               mask  = 0xf0;
+    const unsigned int          n_bcd = (sizeof(BCD)*2);
+    unsigned char const * const bytes = (unsigned char*)&bcd;
+
+    // we want to start at the Most-Significant-Nibble
+    // of the BCD representation since then our polyexpansion is much
+    // simpler
+    for(digit=0, n=Number(0); ((old_n=n)==n) && digit<n_bcd; digit++, mask=~mask, shift=!shift) {
+        if( (decoded_digit=Number(((bytes[(n_bcd-digit-1)/2]&mask)>>(shift?4:0))))>9 )
+            THROW_A_BCD(not_a_BCD_digit, "Only digits 0..9 are considered valid BCD digits");
+        n = (n*10) + decoded_digit;
+        if( n<old_n )
+            THROW_A_BCD(argument_too_big, "The BCD-encoded value was too big to fit into the destination");
+    }
+    return;
+}
+
+// decode an array of bytes (holding 2 BCD digits each) into
+// an array of individual digits. Caller is responsible that digits points
+// at an array of at least ndigit elements
+template <typename Number>
+void unbcd(unsigned char const* bcd, Number* digits, const unsigned int ndigit) {
+    bool          shift = true;
+    unsigned char mask  = 0xf0;
+
+    for(unsigned int i=0; i<ndigit; i++, mask=~mask, shift=!shift, (((i&0x1)==0)?bcd++:bcd))
+        if( (digits[i] = ((*bcd)&mask)>>(shift?4:0))>9 )
+            THROW_A_BCD(not_a_BCD_digit, "Only digits 0..9 are considered valid BCD digits");
+    return;
+}
+
+// decode all of the BCD digits found in bcd into an array of the individual
+// digits. Caller is responsible to make sure that the array pointed to by
+// digits can hold all the digits.
+template <typename BCD, typename Number>
+void unbcd(const BCD bcd, Number* digits) {
+    const unsigned int nbcd = 2*sizeof(BCD);
+    BCD                mask = (((BCD)0xf) << ((nbcd-1)*4));
+    
+    for(unsigned int i=0; i<nbcd; i++, mask>>=4)
+        if( (digits[i] = (bcd&mask)>>((nbcd - i - 1)*4))>9 )
+            THROW_A_BCD(not_a_BCD_digit, "Only digits 0..9 are considered valid BCD digits");
+    return;
+}
+
+// Inline UNBCD "packed" BCD digits into an array of individual digits.
+template <size_t>
+struct size_check { };
+
+template <>
+struct size_check<(size_t)1> {
+    typedef unsigned int  packed_datatype_is_not_1byte_wide;
+};
+#define UNBCD(packed, unpacked, ndigits) \
+    do { \
+        size_check<sizeof(*packed)>::packed_datatype_is_not_1byte_wide i = 0, j = 0;\
+        while( ndigits>1 ) { \
+            unpacked[i+1] = packed[j  ]&0xf;\
+            unpacked[i  ] = packed[j  ]>>4;\
+            i+=2; j++; ndigits-=2; \
+        } \
+        if( ndigits ) \
+            unpacked[i++] = packed[j]>>4;\
+    } while( 0 );
+
+
+#endif
