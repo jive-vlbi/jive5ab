8193f1700d86b574dc11b074834eac99e00be2b5
jive_cc <>
2013-01-10 16:07:03 +0000

    BE: thread functions for writting to/reading from interchain queues and global functions to initialize these queues

--- evlbi5a/buffering.cc
+++ evlbi5a/buffering.cc
@@ -6,29 +6,81 @@ using namespace std;
 
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
-// global queue object used to communicate between 2 chains (written into fifo_queue_writer, read from by queue_reader)
-bqueue<block> interchain_queue;
+// global queue object used to communicate between 2 chains (written into by queue_writers, read from by queue_readers)
+struct interchain_queues_type {
+    bqueue<block>* queues;
+    unsigned int   len;
+    interchain_queues_type() : queues(NULL), len(0) {}
+    ~interchain_queues_type() {
+        delete[] queues;
+    }
+};
+static interchain_queues_type interchain;
 
-queue_writer_args::queue_writer_args() : rteptr(NULL), pool(NULL), disable(false) {}
-queue_writer_args::queue_writer_args(runtime* r) : rteptr(r), pool(NULL), disable(false) {}
-queue_writer_args::~queue_writer_args() {
+void init_interchain_queues(unsigned int len) {
+    ASSERT_COND( interchain.queues == NULL );
+    interchain.queues = new bqueue<block>[len];
+    interchain.len = len;
+}
+
+bqueue<block>& get_interchain_queue(unsigned int index) {
+    ASSERT_COND( index < interchain.len );
+    return interchain.queues[index];
+}
+
+fifo_queue_writer_args::fifo_queue_writer_args() : rteptr(NULL), pool(NULL), disable(false) {}
+fifo_queue_writer_args::fifo_queue_writer_args(runtime* r) : rteptr(r), pool(NULL), disable(false) {}
+fifo_queue_writer_args::~fifo_queue_writer_args() {
     // clean up
-    if (disable) {
-        interchain_queue.disable();
+    if ( disable ) {
+        for ( unsigned int index = 0;
+              index < interchain.len;
+              index++ ) {
+            interchain.queues[index].disable();
+        }
     }
     delete pool;
 }
 
-queue_reader_args::queue_reader_args() : rteptr(NULL), pool(NULL), run(false) {}
-queue_reader_args::queue_reader_args(runtime* r) : rteptr(r), pool(NULL), run(false) {}
+queue_writer_args::queue_writer_args() : rteptr(NULL), disable(false) {}
+queue_writer_args::queue_writer_args(runtime* r) : rteptr(r), disable(false) {}
+queue_writer_args::~queue_writer_args() {
+    // clean up
+    if ( disable ) {
+        for ( unsigned int index = 0;
+              index < interchain.len;
+              index++ ) {
+            interchain.queues[index].disable();
+        }
+    }
+}
+
+queue_reader_args::queue_reader_args() : rteptr(NULL), pool(NULL), run(false), reuse_blocks(false), finished(false) {}
+queue_reader_args::queue_reader_args(runtime* r) : rteptr(r), pool(NULL), run(false), reuse_blocks(false), finished(false) {}
 queue_reader_args::~queue_reader_args() {
     delete pool;
 }
 void queue_reader_args::set_run(bool val) {
     run = val;
 }
+bool queue_reader_args::is_finished() {
+    return finished;
+}
 
-void fifo_queue_writer(outq_type<block>*, sync_type<queue_writer_args>* args) {
+queue_forker_args::queue_forker_args() : rteptr(NULL), disable(false) {}
+queue_forker_args::queue_forker_args(runtime* r) : rteptr(r), disable(false) {}
+queue_forker_args::~queue_forker_args() {
+    // clean up
+    if ( disable ) {
+        for ( unsigned int index = 0;
+              index < interchain.len;
+              index++ ) {
+            interchain.queues[index].disable();
+        }
+    }
+}
+
+void fifo_queue_writer(outq_type<block>*, sync_type<fifo_queue_writer_args>* args) {
     // If we must empty the FIFO we must read the data to somewhere so
     // we allocate an emergency block, 
     // for emptying the fifo if downstream isn't fast enough
@@ -43,7 +95,7 @@ void fifo_queue_writer(outq_type<block>*, sync_type<queue_writer_args>* args) {
     runtime*           rteptr = args->userdata->rteptr;
     SSHANDLE           sshandle;
     DWORDLONG          fifolen;
-    queue_writer_args* qwargs = args->userdata;
+    fifo_queue_writer_args* qwargs = args->userdata;
 
     RTEEXEC(*rteptr, rteptr->sizes.validate());
     const unsigned int blocksize = qwargs->rteptr->sizes[constraints::blocksize];
@@ -53,7 +105,11 @@ void fifo_queue_writer(outq_type<block>*, sync_type<queue_writer_args>* args) {
               qwargs->pool = new blockpool_type(blocksize, 16) );
 
     // enable the queue between the chains, remember to disable it when done
-    interchain_queue.resize_enable_push(1024);
+    for ( unsigned int index = 0;
+          index < interchain.len;
+          index++ ) {
+        interchain.queues[index].resize_enable_push(1024);
+    }
     qwargs->disable = true;
 
     // Request a counter for counting into
@@ -107,20 +163,62 @@ void fifo_queue_writer(outq_type<block>*, sync_type<queue_writer_args>* args) {
 
         XLRCALL( ::XLRReadFifo(sshandle, (READTYPE*)b.iov_base, b.iov_len, 0) );
 
-        interchain_queue.try_push(b);
-
         // indicate we've read another 'blocksize' amount of
         // bytes from the StreamStor
         counter += b.iov_len;
+
+        for ( unsigned int index = 0;
+              index < interchain.len;
+              index++ ) {
+            interchain.queues[index].try_push(b);
+        }
+
     }
     DEBUG(0, "fifo_queue_writer: stopping" << endl);
 }
 
+void queue_writer(inq_type<block>* inq, sync_type<queue_writer_args>* args) {
+    ASSERT_COND( args && args->userdata && args->userdata->rteptr );
+    queue_writer_args* qwargs = args->userdata;
+    runtime* rteptr = qwargs->rteptr;
+
+    // Request a counter for counting into
+    RTEEXEC(*rteptr,
+            rteptr->statistics.init(args->stepid, "MemWriter", 0));
+    volatile int64_t& counter( rteptr->statistics.counter(args->stepid) );
+
+    // enable the queue between the chains, remember to disable it when done
+    for ( unsigned int index = 0;
+          index < interchain.len;
+          index++ ) {
+        interchain.queues[index].resize_enable_push(1024);
+    }
+    args->userdata->disable = true;
+
+    do {
+        block b;
+        if ( !inq->pop(b) ) {
+            break;
+        }
+        counter += b.iov_len;
+        for ( unsigned int index = 0;
+              index < interchain.len;
+              index++ ) {
+            interchain.queues[index].try_push(b);
+        }
+    } while(true);
+
+}
+
 void queue_reader(outq_type<block>* outq, sync_type<queue_reader_args>* args) {
     // Make sure we're not 'made out of 0-pointers'
-    ASSERT_COND( args && args->userdata && args->userdata->rteptr );
+    ASSERT_COND( args && 
+                 args->userdata && 
+                 args->userdata->rteptr && 
+                 args->userdata->rteptr->interchain_source_queue );
     queue_reader_args* qargs = args->userdata;
     runtime* rteptr = qargs->rteptr;
+    bqueue<block>* interchain_queue = rteptr->interchain_source_queue;
     
     RTEEXEC(*rteptr, rteptr->sizes.validate());
     const unsigned int out_blocksize = rteptr->sizes[constraints::blocksize];
@@ -154,7 +252,7 @@ void queue_reader(outq_type<block>* outq, sync_type<queue_reader_args>* args) {
             rteptr->transfersubmode.clr(wait_flag).set(run_flag));
 
     while (!stop) {
-        interchain_queue.enable_pop_only();
+        interchain_queue->enable_pop_only();
         if (!qargs->run || args->cancelled) break;
         block b;
         unsigned int read_index = 0; // first unused byte in first block in buffer
@@ -165,7 +263,7 @@ void queue_reader(outq_type<block>* outq, sync_type<queue_reader_args>* args) {
         while (interchain_queue_enabled && !stop) {
             // read from queue till we have enough bytes to form a new output block
             if (bytes_in_buffer < out_blocksize) {
-                if (!interchain_queue.pop(b)) {
+                if (!interchain_queue->pop(b)) {
                     interchain_queue_enabled = false;
                 }
                 else {
@@ -177,7 +275,7 @@ void queue_reader(outq_type<block>* outq, sync_type<queue_reader_args>* args) {
             else {
                 ASSERT_COND(!blocks_buffer.empty());
                 b = blocks_buffer.front();
-                if (b.iov_len - read_index >= out_blocksize) {
+                if ( (qargs->reuse_blocks) && (b.iov_len - read_index >= out_blocksize)) {
                     // can request a slice of the current block
                     if (!outq->push(b.sub(read_index, out_blocksize))) {
                         stop = true;
@@ -225,11 +323,106 @@ void queue_reader(outq_type<block>* outq, sync_type<queue_reader_args>* args) {
         }
     }
     DEBUG(0, "queue_reader: stopping" << endl);
+    qargs->finished = true;
 }
 
-void void_step(inq_type<block>*, sync_type<void_step_args>*) {}
+void stupid_queue_reader(outq_type<block>* outq, sync_type<queue_reader_args>* args) {
+    // Make sure we're not 'made out of 0-pointers'
+    ASSERT_COND( args && 
+                 args->userdata && 
+                 args->userdata->rteptr && 
+                 args->userdata->rteptr->interchain_source_queue );
+    queue_reader_args* qargs = args->userdata;
+    runtime* rteptr = qargs->rteptr;
+    bqueue<block>* interchain_queue = rteptr->interchain_source_queue;
+    
+     // Request a counter for counting into
+    RTEEXEC(*rteptr,
+            rteptr->statistics.init(args->stepid, "StupidQReader", 0));
 
-void cancel_queue_readers(queue_reader_args* args) {
+    // Provide unconditional and unlocked access to a counter
+    volatile int64_t& counter( rteptr->statistics.counter(args->stepid) );
+
+    bool stop;
+    args->lock();
+    while( !qargs->run && !args->cancelled )
+        args->cond_wait();
+    // Ah. At least one of the conditions was met.
+    stop = args->cancelled;
+    args->unlock();
+
+    // Premature cancellation?
+    if( stop ) {
+        DEBUG(0, "stupid queue reader: cancelled before start" << endl);
+        return;
+    }
+
+    // Now we can indicate we're running!
+    RTEEXEC(*rteptr,
+            rteptr->transfersubmode.clr(wait_flag).set(run_flag));
+    
+    bool interchain_queue_enabled;
+    while (!stop) {
+        interchain_queue->enable_pop_only();
+        if (!qargs->run || args->cancelled) break;
+        interchain_queue_enabled = true;
+        block b;
+        while (interchain_queue_enabled && !stop) {
+            if (!interchain_queue->pop(b)) {
+                interchain_queue_enabled = false;
+            }
+            else {
+                if (!outq->push(b)) {
+                    stop = true;
+                }
+                else {
+                    counter += b.iov_len;
+                }
+            }
+        }
+    }
+    DEBUG(0, "stupid_queue_reader: stopping" << endl);
+    qargs->finished = true;
+}
+
+void void_step(inq_type<block>*) {}
+
+void queue_forker(inq_type<block>* inq, outq_type<block>* outq, sync_type<queue_forker_args>* args) {
+    ASSERT_COND( args && args->userdata && args->userdata->rteptr );
+
+    runtime* rteptr = args->userdata->rteptr;
+
+    // Request a counter for counting into
+    RTEEXEC(*rteptr,
+            rteptr->statistics.init(args->stepid, "Forker", 0));
+    volatile int64_t& counter( rteptr->statistics.counter(args->stepid) );
+
+    // enable the queue between the chains, remember to disable it when done
+    for ( unsigned int index = 0;
+          index < interchain.len;
+          index++ ) {
+        interchain.queues[index].resize_enable_push(1024);
+    }
+    args->userdata->disable = true;
+
+    do {
+        block b;
+        if (!inq->pop(b) || !outq->push(b)) {
+            break;
+        }
+        counter += b.iov_len;
+        for ( unsigned int index = 0;
+              index < interchain.len;
+              index++ ) {
+            interchain.queues[index].try_push(b);
+        }
+    } while(true);
+
+}
+
+void cancel_queue_reader(queue_reader_args* args) {
     args->set_run(false);
-    interchain_queue.disable_pop();
+    ASSERT_COND( args->rteptr && args->rteptr->interchain_source_queue );
+    args->rteptr->interchain_source_queue->disable_pop();
 }
+
--- evlbi5a/buffering.h
+++ evlbi5a/buffering.h
@@ -8,6 +8,21 @@
 #include <bqueue.h>
 #include <threadfns.h>
 
+// functions to setup queues used by thread functions below to communicate data between runtimes/chains
+void init_interchain_queues(unsigned int len);
+bqueue<block>& get_interchain_queue(unsigned int index);
+
+struct fifo_queue_writer_args {
+    fifo_queue_writer_args();
+    fifo_queue_writer_args(runtime* r);
+
+    ~fifo_queue_writer_args();
+
+    runtime*        rteptr;
+    blockpool_type* pool;
+    bool            disable;
+};
+
 struct queue_writer_args {
     queue_writer_args();
     queue_writer_args(runtime* r);
@@ -15,7 +30,6 @@ struct queue_writer_args {
     ~queue_writer_args();
 
     runtime*        rteptr;
-    blockpool_type* pool;
     bool            disable;
 };
 
@@ -23,6 +37,8 @@ struct queue_reader_args {
     runtime* rteptr;
     blockpool_type*    pool;
     bool run;
+    bool reuse_blocks; // reuse blocks from interchain queue in this chain (if true), or always make a local copy (if false)
+    bool finished;
 
     queue_reader_args();
     queue_reader_args(runtime* r);
@@ -30,15 +46,27 @@ struct queue_reader_args {
     ~queue_reader_args();
 
     void set_run(bool val);
+    bool is_finished();
 };
 
-struct void_step_args {
+struct queue_forker_args {
+    runtime* rteptr;
+    bool     disable;
+    queue_forker_args();
+    queue_forker_args(runtime* r);
+    ~queue_forker_args();
 };
 
-void fifo_queue_writer(outq_type<block>*, sync_type<queue_writer_args>* );
+void fifo_queue_writer(outq_type<block>*, sync_type<fifo_queue_writer_args>* );
+void queue_writer(inq_type<block>*, sync_type<queue_writer_args>* );
+
+// two version of queue reader, the first resizes to the requested blocksize
+// the stupid version does not
 void queue_reader(outq_type<block>*, sync_type<queue_reader_args>* );
-void void_step(inq_type<block>*, sync_type<void_step_args>*);
+void stupid_queue_reader(outq_type<block>*, sync_type<queue_reader_args>* );
+void void_step(inq_type<block>*);
+void queue_forker(inq_type<block>*, outq_type<block>*, sync_type<queue_forker_args>*);
 
-void cancel_queue_readers(queue_reader_args*);
+void cancel_queue_reader(queue_reader_args*);
 
 #endif
